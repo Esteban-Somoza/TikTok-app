4fea8b7648398b9a063ec2f9c7dccf34a78164ca
Esteban-Somoza <estfersomoza@gmail.com>
2023-02-28 15:59:08 -0300

    upload file form

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,8 +12,8 @@ import { createOrGetUser } from 'utils'
 import useAuthStore from 'store/authStore'
 
 function Navbar() {
-  const {userProfile, addUser} = useAuthStore();
-  
+  const { userProfile, addUser, removeUser } = useAuthStore();
+
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
       <Link href='/'>
@@ -26,10 +26,35 @@ function Navbar() {
 
       <div>
         {userProfile?.userName ? (
-          <div>{userProfile.userName}</div>
+          <div className='flex gap-5 md:gap-10'>
+            <Link href='/upload'>
+              <button className='border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2'>
+                <IoMdAdd className='text-xl' /> {` `}
+                <span className='hidden md:block'>Upload</span>
+              </button>
+            </Link>
+            {userProfile?.image && (
+              <Link href='/'>
+                <>
+                  <Image src={userProfile.image} 
+                  width={40} 
+                  height={40} 
+                  className='rounded-full cursor-pointer' 
+                  alt='profile shoot' />
+                </>
+              </Link>
+            )}
+            <button type='button' className='px-2' onClick={() => {
+              console.log('logout');
+              googleLogout();
+              removeUser();
+            }}>
+              <AiOutlineLogout color='red' fontSize={21}/>
+            </button>
+          </div>
         ) : (
           <div>
-            <GoogleLogin onSuccess={(response) => createOrGetUser(response, addUser)} onError={() => console.log('error')}/>
+            <GoogleLogin onSuccess={(response) => createOrGetUser(response, addUser)} onError={() => console.log('error')} />
           </div>
         )
         }
--- store/authStore.ts
+++ store/authStore.ts
@@ -7,7 +7,12 @@ import axios from 'axios'
 const authStore = (set: any) => ({
     userProfile: null,
 
-    addUser: (user: any) => set({ userProfile: user })
+    addUser: (user: any) => set({ userProfile: user }),
+    removeUser: () => {
+        console.log('remove');
+        set({ userProfile: null })
+
+    }
 })
 
 const useAuthStore = create(persist(authStore, {
